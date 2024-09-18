84a6eb1fbc5978fbd6bc01395461f5200ac31dff
Vương Phương Thảo <125387948+pthao12@users.noreply.github.com>
2024-09-18 13:20:29 +0700

    fix Chatbot UI

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import * as SpeechSDK from 'microsoft-cognitiveservices-speech-sdk';
+import { FaMicrophone } from "react-icons/fa";
+import { IoSend } from "react-icons/io5";
 
 const AZURE_SUBSCRIPTION_KEY = process.env.REACT_APP_AZURE_SUBSCRIPTION_KEY;
 const AZURE_REGION = process.env.REACT_APP_AZURE_REGION;
@@ -51,29 +53,48 @@ const Chatbot = () => {
   };
 
   return (
-    <div className="chat-container">
-      <div className="message-box">
+    <div className="flex flex-col h-[82svh] max-w-lg mt-3">
+      <div className="messages flex flex-col space-y-4 p-3 flex-grow overflow-y-auto">
         {messages.map((msg, idx) => (
-          <div key={idx} className={`message ${msg.sender === 'user' ? 'user-message' : 'bot-message'}`}>
-            {msg.text}
+          <div key={idx}>
+            {msg.sender === 'user' ? (
+              <div className="chat-message">
+                <div className="flex items-end justify-end">
+                  <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-end">
+                    <span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-dark-green text-white text-xl">
+                      {msg.text}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="chat-message">
+                <div className="flex items-end">
+                  <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
+                    <span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600 text-xl">
+                      {msg.text}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         ))}
-        {isLoading && <div className="typing-indicator">...</div>}
       </div>
-
-      <div className="input-box">
+  
+      <div className="input-box flex w-full p-2 bg-white border-t border-gray-300">
         <input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="Type a message or speak"
-          className="input-field"
+          className="flex-grow p-2 border border-gray-300 rounded-l-lg"
         />
-        <button onClick={startListening} className="voice-icon">🎤</button>
-        <button onClick={sendMessage} className="send-button">Send</button>
+        <FaMicrophone onClick={startListening} className="text-gray-600 text-3xl "/>
+        <IoSend onClick={sendMessage} className="text-blue-500 text-3xl ml-2"/>
       </div>
     </div>
   );
-};
+};  
 
 export default Chatbot;
